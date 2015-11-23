09086fa3283da2afa1e7d3f97792a97597868ad6
Anya Tran <anhtran9@ccs.neu.edu>
2015-11-23 11:51:42 -0500

    jasmine tests

--- static/js/main.js
+++ static/js/main.js
@@ -1,92 +1,131 @@
 $(document).ready(function () {
 
   //Navigation Menu Slider
-  $('#nav-expander').on('click', function (e) {
-    e.preventDefault();
-      $('.sidebar').toggleClass('sidebar-expanded');
+    function toggleSidebar() {
+        $('.sidebar').toggleClass('sidebar-expanded');
+        if($('.sidebar-expanded').length <= 0) {
+            $('.bottombar-expanded > nav').width('97%');
+            $('.bottombar-expanded > nav').css('margin-left', '-26%');
+        }
+        if($('.sidebar-expanded').length > 0) {
+            $('.bottombar-expanded > nav').width('71%');
+            $('.bottombar-expanded > nav').css('margin-left', '0px');
+        }
+    }
+
+    /*$('#nav-expander').on('click', function () {
+        if($('.sidebar-expanded').length <= 0) {
+            $('.bottombar-expanded > nav').width('97%');
+            $('.bottombar-expanded > nav').css('margin-left', '-26%');
+        }
+        if($('.sidebar-expanded').length > 0) {
+            $('.bottombar-expanded > nav').width('71%');
+            $('.bottombar-expanded > nav').css('margin-left', '0px');
+        }
     });
-
-  $('#nav-expander').on('click', function () {
-    if($('.sidebar-expanded').length <= 0) {
-      $('.bottombar-expanded > nav').width('97%');
-      $('.bottombar-expanded > nav').css('margin-left', '-26%');
+*/
+    function sidebarCollapse() {
+        $('.bottombar-expanded > nav').width('97%');
+        $('.bottombar-expanded > nav').css('margin-left', '-26%')
+        activateAndResetFields();
     }
-    if($('.sidebar-expanded').length > 0) {
-      $('.bottombar-expanded > nav').width('71%');
-      $('.bottombar-expanded > nav').css('margin-left', '0px');
+    function removeSidebar() {
+        $('.sidebar').removeClass('sidebar-expanded');
+        $('.bottombar-expanded > nav').width('97%');
+        $('.bottombar-expanded > nav').css('margin-left', '-26%');
+        console.log("remove");
     }
-  });
-
-  $('.sidebar-expanded').on('collapse', function(){
-    $('.bottombar-expanded > nav').width('97%');
-    $('.bottombar-expanded > nav').css('margin-left', '-26%')
-    activateAndResetFields();
-  })
-
-  $('.sidebar .nav-close').on('click', function (e) {
-    e.preventDefault();
-    $('.sidebar').removeClass('sidebar-expanded');
-    $('.bottombar-expanded > nav').width('97%');
-    $('.bottombar-expanded > nav').css('margin-left', '-26%');
-  });
-  $('.bottombar .nav-close').on('click', function (e) {
-    e.preventDefault();
-    $('.bottombar-expanded > nav').height('27%');
-    $('.bottombar').removeClass('bottombar-expanded');
-  });
-
-  $('#generate').on('click', function(e) {
-    e.preventDefault();
-    if ($('.bottombar-expanded').length == 0) {
-      $('.bottombar').addClass('bottombar-expanded');
+    function bottombarCollapse() {
+        $('.bottombar-expanded > nav').height('27%');
+        $('.bottombar').removeClass('bottombar-expanded');
     }
-  });
+
+    function generate() {
+        if ($('.bottombar-expanded').length == 0) {
+            $('.bottombar').addClass('bottombar-expanded');
+        }
+    }
 
   // Initialize navgoco with default options
-  $(".main-menu").navgoco({
-    caret: '<span class="caret"></span>',
-    accordion: false,
-    openClass: 'open',
-    save: true,
-    cookie: {
-      name: 'navgoco',
-      expires: false,
-      path: '/'
-    },
-    slide: {
-      duration: 300,
-      easing: 'swing'
+    function initNavgoco() {
+        $(".main-menu").navgoco({
+            caret: '<span class="caret"></span>',
+            accordion: false,
+            openClass: 'open',
+            save: true,
+            cookie: {
+                name: 'navgoco',
+                expires: false,
+                path: '/'
+            },
+            slide: {
+                duration: 300,
+                easing: 'swing'
+            }
+        });
     }
-  });
 
-  $('#drag').on('mousedown', function(e){
-        var $dragable = $(this).parent(),
-            startHeight = $dragable.height(),
-            startWidth = $dragable.width(),
-            pY = e.pageY;
+    function bottombarContent() {
+        if($("#data").css("display") == "inline") {
+            $("#data").css("display", "none");
+            $("#graphs").css("display", "inline");
+        } else {
+            $("#data").css("display", "inline");
+            $("#graphs").css("display", "none");
+        }
+    }
+
+    function main() {
+        $('#nav-expander').on('click', function (e) {
+            e.preventDefault();
+            toggleSidebar();
+        });
+
+        $('.sidebar-expanded').on('collapse', function(){
+            sidebarCollapse();
+        });
         
-        $(document).on('mouseup', function(e){
-            $(document).off('mouseup').off('mousemove');
+        $('.sidebar .nav-close').on('click', function (e) {
+            e.preventDefault();
+            removeSidebar();
         });
-        $(document).on('mousemove', function(me){
-            var my = (me.pageY - pY);
-            
-            $dragable.css({
-                height: startHeight - my, 
-                width:startWidth
-            });
+        
+        $('.bottombar .nav-close').on('click', function (e) {
+            e.preventDefault();
+            bottombarCollapse();
         });
-                
-    });
-	
-		$("#myonoffswitch").on("click", function(e) {            
-				if($("#data").css("display") == "inline") {
-						$("#data").css("display", "none");
-						$("#graphs").css("display", "inline");
-				} else {
-						$("#data").css("display", "inline");
-						$("#graphs").css("display", "none");
-				}
-		});
+        
+        $('#generate').on('click', function(e) {
+            e.preventDefault();
+            generate();
+        });
+        
+        initNavgoco();
 
+        $('#drag').on('mousedown', function(e){
+            var $dragable = $(this).parent(),
+                startHeight = $dragable.height(),
+                startWidth = $dragable.width(),
+                pY = e.pageY;
+
+            $(document).on('mouseup', function(e){
+                $(document).off('mouseup').off('mousemove');
+            });
+            $(document).on('mousemove', function(me){
+                var my = (me.pageY - pY);
+
+                $dragable.css({
+                    height: startHeight - my,
+                    width:startWidth
+                });
+            });
+
+        });
+        $("#myonoffswitch").on("click", function() {
+            bottombarContent();
+        });
+    }
+    main();
 });
+
+
