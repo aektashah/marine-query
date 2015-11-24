42c7bb430b1194c3c00c14e7149b80decb70fac3
Anya Tran <anhtran9@ccs.neu.edu>
2015-11-24 16:27:55 -0500

    removed comments

--- static/test/test.js
+++ static/test/test.js
@@ -1,36 +1,22 @@
+/**
+* Unit test for front end
+**/
 describe("show sidebar by clicking FILTER", function() {
     var spyEvent;
     var fixture;
     beforeEach(function() {
         loadFixtures("../../../index.html");
-        fixture = $("#nav-expander");//[0].outerHTML;
-        console.log(fixture);
+    });
+    it ("should not have a sidebar-expanded class", function() {
+        expect($('.sidebar')).not.toHaveClass("sidebar-expanded");
     });
     it ("should show the sidebar", function() {
         spyEvent = spyOnEvent("#nav-expander", "click");
-        $("#nav-expander")[0].click();
+        fixture = $("#nav-expander");//[0].outerHTML;
+        var filterButton = $("#nav-expander")[0];
+        filterButton.click();
         expect("click").toHaveBeenTriggeredOn("#nav-expander");
         expect(spyEvent).toHaveBeenTriggered();
+        expect($('.sidebar')).toHaveClass("sidebar-expanded");
     });
 });
-/*
-describe("sidebar remove", function() {
-    var spyEvent;
-    
-    it("should remove sidebar class", function() {
-        $(".sidebar .nav-close").trigger("click");
-        expect(".sidebar")
-    });
-    
-}*/
-/*
-describe("toggle width and margine-length", function() {
-    beforeEach(function() {
-        $("#nav-expander").trigger("click");
-    });
-    it ("should set width to 97%", function() {
-        console.log($(".bottombar-expanded > nav"));
-        expect($('.bottombar-expanded > nav')).toHaveCss({width: '97%'});
-    });
-});
-*/
