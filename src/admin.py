de28a71f92235d8090ef9cbb27f620b1da9441ca
sherry <son.sherrysoomin92@gmail.com>
2015-12-05 17:12:02 -0500

    redirect from upload, flash messages added

--- src/admin.py
+++ src/admin.py
@@ -2,7 +2,7 @@
     Provides the objects necessary to customize the administrative
     interface, and manipulate the database through a GUI
 """
-from flask import request
+from flask import request, flash, redirect, url_for
 from flask.ext.security import current_user, login_required
 from flask.ext.superadmin import AdminIndexView, BaseView, model, expose
 from werkzeug import secure_filename
@@ -35,11 +35,13 @@ class UploadView(AuthMixin, BaseView):
         if request.method == "POST":
             data = request.files["data"]
             Device.add_from_file(data.stream.read(), request.form["dev"])
-            return "hello" 
+            flash("Successfully uploaded data!")
+            return redirect (url_for('uploadview.index', self=self))
+    	# flash("Get the fuck out!")
+	return redirect (url_for('uploadview.index', self=self))
 
 # Subclass of a model view which requires authentication
 class AuthModelView(AuthMixin, model.ModelAdmin):
-
     @expose('/add/', methods=('GET', 'POST'))
     def add(self):
         return super(AuthModelView, self).add()
